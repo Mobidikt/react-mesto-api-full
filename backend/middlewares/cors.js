d621af3036ad7507a913e342552d7e15ad221159
Kirill Mecker <ghost3299@yandex.ru>
2020-11-23 18:49:20 +0400

    feat: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,8 +11,10 @@ const allowedCors = [
 module.exports = async (req, res, next) => {
   const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
   if (allowedCors.includes(origin)) {
+    console.log('if', origin);
     // Проверяем, что значение origin есть среди разрешённых доменов
     res.header('Access-Control-Allow-Origin', origin);
   }
+  console.log('else', origin);
   return next();
 };
