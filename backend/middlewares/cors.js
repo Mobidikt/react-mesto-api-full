4c608d1f315c710ad6f462ca1efc63d38a5e775e
Kirill Mecker <ghost3299@yandex.ru>
2020-11-23 19:32:38 +0400

    feat: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,15 +8,31 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
+// module.exports = async (req, res, next) => {
+//   const { origin } = req.headers;
+//   console.log(res.headers); // Записываем в переменную origin соответствующий заголовок
+//   if (allowedCors.includes(origin)) {
+//     console.log('if', origin);
+//     // Проверяем, что значение origin есть среди разрешённых доменов
+//     res.header('Access-Control-Allow-Origin', origin); //allowedCors);
+//   }
+//   console.log('else', origin);
+//   console.log(res.header);
+//   return next();
+// };
+
 module.exports = async (req, res, next) => {
   const { origin } = req.headers;
   console.log(res.headers); // Записываем в переменную origin соответствующий заголовок
   if (allowedCors.includes(origin)) {
-    console.log('if', origin);
-    // Проверяем, что значение origin есть среди разрешённых доменов
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+
+    console.log(res.header);
   }
-  console.log('else', origin);
-  console.log(res.header);
   return next();
 };
