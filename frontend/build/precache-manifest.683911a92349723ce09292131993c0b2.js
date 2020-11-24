7013d100c9aa8306fa59ea4b6d048bb48755d8f5
Kirill Mecker <ghost3299@yandex.ru>
2020-11-24 21:27:55 +0400

    feat: controllers

--- frontend/build/precache-manifest.683911a92349723ce09292131993c0b2.js
+++ frontend/build/precache-manifest.683911a92349723ce09292131993c0b2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ae4f33aab1ae5a764c982784f4e9eb0a",
+    "revision": "4c1ed0347cbd0c5429fb93d393458144",
     "url": "/index.html"
   },
   {
-    "revision": "1c0039c878087d6f0ef3",
+    "revision": "3c3e7af7e3c38e3ac8de",
     "url": "/static/css/main.6f3acc26.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ca8e7612.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1c0039c878087d6f0ef3",
-    "url": "/static/js/main.2475998a.chunk.js"
+    "revision": "3c3e7af7e3c38e3ac8de",
+    "url": "/static/js/main.2c9a7e35.chunk.js"
   },
   {
     "revision": "9faa06e49578191d5a13",
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://api.rtxmobidikt.students.nomoreparties.co';
-// export const API_URL = 'http://localhost:3000';
+// export const API_URL = 'https://api.rtxmobidikt.students.nomoreparties.co';
+export const API_URL = 'http://localhost:3000';
